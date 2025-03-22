5c897f888639b5b82b3ad2d40a61954c007d4490
wmduggan <wmduggan@gmail.com>
2025-03-22 19:52:57 +0000

    added path logic for grocery list import. need to include grocery list provider auth before updating.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
+import { GroceryListProvider } from "./context/GroceryListContext";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
